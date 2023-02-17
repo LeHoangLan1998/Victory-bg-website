91ec00a2565079da6118d0acbf16e4cb670669e0
LeHoangLan1998 <lehoanglan1998@gmail.com>
2023-02-17 14:40:09 +0200

    replaced slick-slider with keen-slider

--- src/components/CarouselComp.jsx
+++ src/components/CarouselComp.jsx
@@ -1,39 +1,67 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from 'react-slick';
 import { Button } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
+import classes from './CarouselComp.module.css';
+
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react';
+
 
 
 const CarouselComp = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        /* fade: true, */
-        autoplay: true,
-        autospeed: 1500,
-        /* pauseOnHover: true */
-    };
+    //keen-slider
+    const [sliderRef] = useKeenSlider(
+        {
+            loop: true,
+        },
+        [
+            (slider) => {
+                let timeout
+                let mouseOver = false
+                function clearNextTimeout() {
+                    clearTimeout(timeout)
+                }
+                function nextTimeout() {
+                    clearTimeout(timeout)
+                    if (mouseOver) return
+                    timeout = setTimeout(() => {
+                        slider.next()
+                    }, 3000)
+                }
+                slider.on("created", () => {
+                    slider.container.addEventListener("mouseover", () => {
+                        mouseOver = true
+                        clearNextTimeout()
+                    })
+                    slider.container.addEventListener("mouseout", () => {
+                        mouseOver = false
+                        nextTimeout()
+                    })
+                    nextTimeout()
+                })
+                slider.on("dragStarted", clearNextTimeout)
+                slider.on("animationEnded", nextTimeout)
+                slider.on("updated", nextTimeout)
+            },
+        ]
+    )
 
     return (
-        <div className='carousel-container'>
-            <Slider {...settings}>
+        <div className={classes.carouselContainer}>
 
+            <div ref={sliderRef} className="keen-slider" style={{maxHeight: "85vh"}}>
 
-                {/* <div className="carousel-1"></div> */}
-                <Image fluid src="src\assets\pexels-neosiam-603022.jpg"></Image>
+                <div className="keen-slider__slide">
+                    <Image fluid src="src\assets\pexels-neosiam-603022.jpg"></Image>
+                </div>
 
-                {/* <div className="carousel-2"></div */}
-                <Image fluid src="src\assets\pexels-kai-pilger-996329.jpg"></Image>
+                <div className="keen-slider__slide">
+                    <Image fluid src="src\assets\pexels-kai-pilger-996329.jpg"></Image>
+                </div>
 
-                {/* <div className="carousel-3"></div> */}
-                <Image fluid src="src\assets\pexels-terje-sollie-298863.jpg"></Image>
-
-            </Slider >
+                <div className="keen-slider__slide">
+                    <Image fluid src="src\assets\pexels-terje-sollie-298863.jpg"></Image>
+                </div>
+            </div>
             <div className="flex-container" style={{ position: "absolute", top: "40%", width: '100%', flexDirection: "column" }}>
                 <p style={{ fontSize: "4rem" }}>Добре дошли на Victory</p>
                 <p style={{ fontSize: "2rem" }}>дънки, панталони и дрехи във всякакви размери</p>
