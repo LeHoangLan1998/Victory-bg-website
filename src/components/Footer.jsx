488f3eef25a5f258f5c88a31896021ec42c94a49
LeHoangLan1998 <lehoanglan1998@gmail.com>
2023-03-05 22:00:08 +0200

    minor fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,15 @@
-const Footer = () => {
-
-    return (
-        <div className="footer-style">
-            <div className="footer-flex">
-                <p className="footer-p">Контакти: София, ул. Петър Панайотов 12</p>
-                <p className="footer-p">Телефон: +359 87 8223373</p>
-                <p className="footer-p">Email: info@victory-bg.com</p>
-            </div>
-
-            <div className="footer-flex">
-                <p className="footer-p">2022 Copyright Victory-bg Ltd.</p>
-                <p className="footer-p">Created by LHL Ltd.</p>
-=======
 import classes from "./Footer.module.css";
 
 const Footer = () => {
 
-    return(
+    return (
         <div className={classes.footer}>
             <div>
                 <h3>zamn</h3>
-                <p>ala bala</p>
-                <p>portokala</p>
+                <ul>
+                    <li><p>ala bala</p></li>
+                    <li><p>portokala</p></li>
+                </ul>
             </div>
 
             <div>
